fe131270ae903c963722db33e18449a347b35394
JackieMbutu <32986217+JackieMbutu@users.noreply.github.com>
2023-04-10 12:54:49 +0300

    Update CodeGenerator.py

--- CodeGenerator/CodeGenerator.py
+++ CodeGenerator/CodeGenerator.py
@@ -5,118 +5,46 @@ Created on Tue Feb  7 19:41:00 2023
 @author: Mbutu
 """
 
-y = input("Type in Secret Code:")
-if y == "Bingo!":
-    x = input("Tyoe E for Encode or D for Decode:")
-    if x== "E":
-        m = input("Write words to be Encoded:")
-        for i in m: 
-            if i=="a":
-                print("z")
-            if i== "b":
-                print("y")
-            if i== "c":
-                print("x")    
-            if i== "d":
-                print("w")
-            if i== "e":
-                print("v")
-            if i== "f":
-                print("u")
-            if i== "g":
-                print("t")
-            if i== "h":
-                print("s")
-            if i== "i":
-                print("r")
-            if i== "j":
-                print("q")
-            if i=="k":
-                print("p")
-            if i== "l":
-                print("o")
-            if i== "m":
-                print("n")    
-            if i== "n":
-                print("m")
-            if i== "o":
-                print("l")
-            if i== "p":
-                print("k")
-            if i== "q":
-                print("j")
-            if i== "r":
-                print("i")
-            if i== "s":
-                print("h")
-            if i== "t":
-                print("g")
-            if i=="u":
-                print("f")
-            if i== "v":
-                print("e")
-            if i== "w":
-                print("d")    
-            if i== "x":
-                print("c")
-            if i== "y":
-                print("b")
-            if i== "z":
-                print("a")
-    if x== "D":
-        m = input("Write words to be Decoded:")
-        for i in m: 
-            if i=="a":
-                print("z")
-            if i== "b":
-                print("y")
-            if i== "c":
-                print("x")    
-            if i== "d":
-                print("w")
-            if i== "e":
-                print("v")
-            if i== "f":
-                print("u")
-            if i== "g":
-                print("t")
-            if i== "h":
-                print("s")
-            if i== "i":
-                print("r")
-            if i== "j":
-                print("q")
-            if i=="k":
-                print("p")
-            if i== "l":
-                print("o")
-            if i== "m":
-                print("n")    
-            if i== "n":
-                print("m")
-            if i== "o":
-                print("l")
-            if i== "p":
-                print("k")
-            if i== "q":
-                print("j")
-            if i== "r":
-                print("i")
-            if i== "s":
-                print("h")
-            if i== "t":
-                print("g")
-            if i=="u":
-                print("f")
-            if i== "v":
-                print("e")
-            if i== "w":
-                print("d")    
-            if i== "x":
-                print("c")
-            if i== "y":
-                print("b")
-            if i== "z":
-                print("a")
-else:
-    print("Error!!")
+import string
+import pandas as pd
+
+#Generate alphabet
+x = list(string.ascii_lowercase)
+#Generate numbers for the alphabet1
+y = [i for i in range(1,27)]
+
+#Function for encoding
+def encode(w): 
+    z = pd.Series(y, index= x)
+    try:
+        for i in w:
+            s = z[i]
+            print(s)
+    except KeyError:
+        print("Use the right format.")
+        
+#Function for decoding  
+def decode(bb):
+    z = pd.Series(x, index= y)
+    try:
+        for i in bb:
+            a = int(i)
+            s = z[a]
+            print(s)
+    except KeyError:
+        print("VALUE OUT OF RANGE!!\nType numbers in the range 1-26!!")
+    except ValueError:
+        print("Use the right format.")
+
+#Word input and decode/encoder choice      
+choice = input("Type E for encode or D for decode:")
+
+
+if choice == 'E':
+        word = input("Give input(lowercase):")
+        encode(word)
+elif choice == 'D':
+        word = input("Give input(split by space):")
+        q = word.split()
+        decode(q)
+    
